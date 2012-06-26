b4639f71fd88755886ed29e550a4eb304bbc69e6
Andreas Vinter-Hviid <andreas.vinterhviid@gmail.com>
2012-06-26 20:41:05 +0200

    The output is now printed in a prettier format

--- csv2tex.hs
+++ csv2tex.hs
@@ -4,6 +4,19 @@ import Text.ParserCombinators.Parsec
 import Control.Monad
 import System.Environment
 
+--Misc
+
+zipWithLong :: a -> (a->a->b) -> [a]->[a]->[b]
+zipWithLong i f [] (b:bs) = f i b : zipWithLong i f [] bs
+zipWithLong i f (a:as) [] = f a i : zipWithLong i f as []
+zipWithLong i f (a:as) (b:bs) = f a b : zipWithLong i f as bs
+zipWithLong _ _ _ _ = []
+
+myDec :: Int -> Int
+myDec x
+	| x > 0 = x-1
+	| otherwise = x
+
 --Parser
 
 csvParser = sepEndBy row (char '\n')
@@ -25,13 +38,21 @@ texFmt inp = case (parse csvParser "" inp) of
 
 --Formatting
 
-fmtLine :: [String] -> String
-fmtLine [] = ""
-fmtLine (l:[]) = l ++ " \\\\\n"
-fmtLine (l:ls) = l ++ " & " ++ (fmtLine ls)
+colSizes :: [[String]] -> [Int]
+colSizes csv = foldr (zipWithLong 0 max . fmap length) [] csv
+
+padString :: Int -> String -> String
+padString 0 [] = []
+padString x [] = ' ' : padString (myDec x) []
+padString x (s:ss) = s : padString (myDec x) ss
+
+fmtLine :: [Int] -> [String] -> String
+fmtLine _ [] = ""
+fmtLine (w:ws) (l:[]) = padString w l ++ " \\\\\n"
+fmtLine (w:ws) (l:ls) = padString w l ++ " & " ++ (fmtLine ws ls)
 
 fmtCsv :: [[String]] -> String
-fmtCsv = concat . fmap fmtLine
+fmtCsv csv = concat $ fmap (fmtLine (colSizes csv)) csv
 
 --Main
 
